5847ad47ee2dcb8a37b4ab91f4f423df489ecaa5
djchikk <f___thatnoise@mac.com>
2025-03-28 11:16:59 -0700

    cleanup: remove unused i18n directory
    - Removed i18n directory and localization files that weren't being used
    
    - Verified the application still works without these files
    
    - Simplified codebase by removing unused internationalization support

--- app/i18n/index.ts
+++
@@ -1,8 +0,0 @@
-const dictionaries = {
-  en: () => import('./locales/en.json').then((module) => module.default),
-  es: () => import('./locales/es.json').then((module) => module.default)
-}
-
-export const getDictionary = async (locale: keyof typeof dictionaries) => {
-  return dictionaries[locale]()
-} 
