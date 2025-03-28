072d17038bdb829789486dd9b8aef1ad63316132
djchikk <f___thatnoise@mac.com>
2025-03-28 10:54:17 -0700

    cleanup: safely remove test route pages
    - Removed page.tsx files from test routes without removing entire directories
    
    - Preserved shared hooks and utility files that might be used by working code
    
    - Verified that the app still works correctly

--- app/test/page.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import EditableComponentsTest from '../components/test/EditableComponentsTest';
-
-export default function TestPage() {
-  return <EditableComponentsTest />;
-} 
